4bb4842d35a9a7eefc3793e4fa4381d6e26c9af2
sanketrmhatre <154064403+sanketrmhatre@users.noreply.github.com>
2024-01-25 23:59:20 +0530

    Add files via upload

---
+++ New folder/about.php
@@ -0,0 +1,66 @@
+<?php include"config.php";?>
+<!DOCTYPE html>
+<html lang="en">
+<?php include"head.php";?>
+<body>
+
+<?php
+	 include"top_nav.php";
+?>
+	
+    <!-- Page Content -->
+    <div class="container" style="margin-top:70px;">
+  <!-- Page Heading/Breadcrumbs -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header  text-primary">About
+                    <small>Blood Bank</small>
+                </h1>
+                <ol class="breadcrumb">
+                    <li><a href="index.php">Home</a>
+                    </li>
+                    <li class="active">About</li>
+                </ol>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Intro Content -->
+        <div class="row">
+            <div class="col-md-6">
+                <img class="img-responsive" src="images/contact.jpg" alt="">
+            </div>
+            <div class="col-md-6">
+                <h2 class="text-primary">About Blood Bank</h2>
+                <p>This is a prototype Project made for better environment and for making every Blood counts perfectly<br>
+
+                    <br>1) Tanmay Aherkar
+                    <br>2) Rahul Ambavne
+                    <br>3) Ganesh Bandekar
+                    <br>4) Afzal Ansari
+                    <br>5) Tejas Patil
+
+</p>
+                
+            </div>
+        </div>
+        <!-- /.row -->
+
+
+        <hr>
+		<?php include"footer.php"; ?>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+   
+
+</body>
+
+</html>
